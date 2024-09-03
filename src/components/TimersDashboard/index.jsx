9e9dd3261ec72c99777e1a1db0d19332da4dbd9f
yuran-nhassengo <yurannhassengo@gmail.com>
2024-09-03 16:38:56 +0200

    update EditableTimerList

--- src/components/TimersDashboard/index.jsx
+++ src/components/TimersDashboard/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { EditableTimerList } from '../TimerList/EditableTimerList'
+import { ToggleableTimerForm } from './ToggleableTimerForm'
 
 export const TimersDashboard = () => {
   return (
     <div>
         <div>
-            
+          <EditableTimerList/>
+          <ToggleableTimerForm isOpen={true}/>
         </div>
       
     </div>
