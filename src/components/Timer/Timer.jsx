886f67f350f785c19d89f5fd68f63a114fb876c5
yuran-nhassengo <yurannhassengo@gmail.com>
2024-09-03 16:35:09 +0200

    add TimerComponent

---
+++ src/components/Timer/Timer.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+export const Timer = ({title,project,elapsed}) => {
+    const elapsedString = elapsed
+  return (
+    <div>
+      <div>
+        <div>
+            {title}
+        </div>
+        <div>
+            {project}
+        </div>
+        <div>
+            <h2>{elapsedString}</h2>
+        </div>
+        <div>
+            <span>
+                editIcon
+            </span>
+            <span>
+                trash icon
+            </span>
+        </div>
+      </div>
+      <div>
+        Start
+      </div>
+    </div>
+  )
+}
+
+
